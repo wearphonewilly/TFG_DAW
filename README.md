b772faae3ab44c6316550bb5a539d2ded63b5966
Willy WearPhone <willy.cardil.wc@gmail.com>
2021-01-15 18:23:18 +0100

    Commit 1:
    Start

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TFG_DAW
 Proyecto Final de DAW
+
+
+## Diario
+
+Día 1 15/01/21
+
+Empezamos el proyecto, se está buscando la idea, empezamos a redactar los items del word y ya están hechos los primeros scripts y htmls
---
+++ php/main.php
@@ -0,0 +1,36 @@
+<?php
+
+echo "<script> console.log('holaa'); </script>";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../styles/css/navbar.css">
+    <script src="../js/navbar.js" defer></script>
+</head>
+
+<body>
+
+    <div class="topnav" id="myTopnav">
+        <a href="./main.php" class="active">Home</a>
+        <a href="#series">Series</a>
+        <a href="#peliculas">Peliculas</a>
+        <a href="#"><i class="fa fa-fw fa-search"></i> Buscar</a>
+        <a href="#about">About</a>
+        <a href="#about">Make it random</a>
+        <a href="./profile.php">Profile</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+</body>
+
+</html>
