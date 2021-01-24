a677b4e4247a55c6f9a2e726d9853e83c14c114f
Willy WearPhone <willy.cardil.wc@gmail.com>
2021-01-24 18:41:51 +0100

    Commit 4:
    Modifying index.html

---
+++ php/DB.php
@@ -0,0 +1,47 @@
+<?php
+
+class DB
+{
+    private $servername = 'localhost';
+    private $username = 'root';
+    private $password = 'root';
+    private $db = 'watchMe';
+    private $conn;
+    private static $instance;
+
+    private function __construct()
+    {
+        // Create connection
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->db);
+
+        // Check connection
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+            echo "NO CONNECTED 🔌";
+        }
+    }
+
+    private function clone()
+    {
+
+    }
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConn()
+    {
+        return $this->conn;
+    }
+
+    public function changeDB($db)
+    {
+        $this->db = $db;
+        $this->conn->select_db($db);
+    }
+}
