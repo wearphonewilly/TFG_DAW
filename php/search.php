caa273d8872767a4fb045dc972c7daa37180af4b
Willy WearPhone <willy.cardil.wc@gmail.com>
2021-02-11 00:02:47 +0100

    Search whitespace query

--- php/search.php
+++ php/search.php
@@ -1,8 +1,12 @@
 <?php
 
 $busquedaQuery = $_POST["busqueda"];
-var_dump(explode(" ", $busquedaQuery));
-echo "<script> console.log('" . $busquedaQuery . "'); </script>";
+var_dump($busquedaQuery);
+
+if (strpbrk($busquedaQuery, ' ') !== false) { // Para que los espacios se cambien por %20 y poder hacer bien la api por get
+    $busquedaQuery = str_replace(" ", "%20", $busquedaQuery);
+    echo $busquedaQuery;
+}
 
 $busquedaUser = file_get_contents('https://api.themoviedb.org/3/search/tv?api_key=f269df40fe8fe735f1ed701a4bfba1df&language=es&page=1&query=' . $busquedaQuery . '&include_adult=false');
 $busquedaUser = json_decode($busquedaUser, true);
