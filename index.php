0b442786e49859fb17682c4b1da98fce1eb9560f
Willy WearPhone <willy.cardil.wc@gmail.com>
2021-02-25 12:53:57 +0100

    No bootstrap

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
@@ -21,6 +22,29 @@
     <!-- Styles -->
     <link rel="stylesheet" href="./styles/css/index.css">
 
+    <style>
+        .custom-shape-divider-top-1614097386 {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            overflow: hidden;
+            line-height: 0;
+            margin-top: 47px;
+        }
+
+        .custom-shape-divider-top-1614097386 svg {
+            position: relative;
+            display: block;
+            width: calc(100% + 1.3px);
+            height: 309px;
+        }
+
+        .custom-shape-divider-top-1614097386 .shape-fill {
+            fill: #836464;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -35,6 +59,15 @@
     </nav>
 
     <section class="intro">
+
+        <div class="custom-shape-divider-top-1614097386">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+                preserveAspectRatio="none">
+                <path
+                    d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                    class="shape-fill"></path>
+            </svg>
+        </div>
         <div class="content">
             <h1>WatchME</h1>
         </div>
@@ -67,14 +100,14 @@
 
     </section>
 
-    <section class="row">
-        <div class="col-md-12">
+    <section>
+        <div class="content">
             <!-- <h2>¿Tienes problemas para elegir una película para la primera cita?</h2> -->
             <h2>¿Tienes problemas para escoger una película?</h2>
             <p><i> <b> Mamá dice que la vida es como una caja de bombones, <br> nunca sabes el que te va a tocar </b>
                 </i> Nuestro sistema seleccionará una por ti</p>
         </div>
-        <div class="col-md-12">
+        <div class="content">
             <img src="./styles/img/forrestGump.png" alt="Forrest Gump by Tom Hanks" width="300" height="400">
         </div>
     </section>
