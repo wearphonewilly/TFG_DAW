e6a8a1ce4ed4ccb83bcf17075e392b5fea8a5a02
wearphonewilly <46891900+wearphonewilly@users.noreply.github.com>
2021-05-18 23:12:40 +0200

    Update valoracionEstrellas.php

--- php/valoracionEstrellas.php
+++ php/valoracionEstrellas.php
@@ -24,11 +24,11 @@
     <h1>VALORACIÓN</h1>
 
     <div class="star-wrapper">
-        <a onclick="puntuacionEstrellas('5', <?php echo $_GET['serieValorar'] ?> )" class="fas fa-star s1"></a>
-        <a onclick="puntuacionEstrellas('4', <?php echo $_GET['serieValorar'] ?>)" class="fas fa-star s2"></a>
-        <a onclick="puntuacionEstrellas('3', <?php echo $_GET['serieValorar'] ?>)" class="fas fa-star s3"></a>
-        <a onclick="puntuacionEstrellas('2', <?php echo $_GET['serieValorar'] ?>)" class="fas fa-star s4"></a>
-        <a onclick="puntuacionEstrellas('1', <?php echo $_GET['serieValorar'] ?>)" class="fas fa-star s5"></a>
+        <a onclick="puntuacionEstrellas('5', <?php echo $_POST['serieValorar'] ?> )" class="fas fa-star s1"></a>
+        <a onclick="puntuacionEstrellas('4', <?php echo $_POST['serieValorar'] ?>)" class="fas fa-star s2"></a>
+        <a onclick="puntuacionEstrellas('3', <?php echo $_POST['serieValorar'] ?>)" class="fas fa-star s3"></a>
+        <a onclick="puntuacionEstrellas('2', <?php echo $_POST['serieValorar'] ?>)" class="fas fa-star s4"></a>
+        <a onclick="puntuacionEstrellas('1', <?php echo $_POST['serieValorar'] ?>)" class="fas fa-star s5"></a>
     </div>
 
     <?php include('../html/scripts.html'); ?>
