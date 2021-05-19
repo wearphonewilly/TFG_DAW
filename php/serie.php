0e9eb9a60fae4884ab129dda52a63db210f32030
Willy WearPhone <willy.cardil.wc@gmail.com>
2021-05-19 15:10:24 +0200

    Twitter button series

--- php/serie.php
+++ php/serie.php
@@ -112,6 +112,7 @@
             </form>
         </div>
 
+        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Estoy viendo una serie en #WatchMe" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
 
     <?php
